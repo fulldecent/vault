b17d8b28b71f34ca117e127cf45737529caeaca1
Mason Fischer <mason@kissr.co>
2017-10-24 11:07:04 -0400

    Store account balances in storage
    Discussion:
    [here](https://github.com/compound-finance/vault/pull/1#issuecomment-338217658)

--- contracts/ETHSavingsAccount.sol
+++ contracts/ETHSavingsAccount.sol
@@ -1,10 +1,17 @@
 pragma solidity ^0.4.4;
 
 contract ETHSavingsAccount {
+  enum Tokens { ETH }
+  mapping(address => mapping(uint256 => uint256)) balances;
   event LedgerEntry(address address_, uint debit, uint credit);
 
   function deposit() payable {
     LedgerEntry(msg.sender, msg.value, 0);
     LedgerEntry(address(this), 0, msg.value);
+    balances[msg.sender][uint(Tokens.ETH)] = balances[msg.sender][uint(Tokens.ETH)] + msg.value;
+  }
+
+  function getBalance(address address_, int tokenType) public returns (uint256) {
+    return balances[address_][uint(tokenType)];
   }
 }
--- test/ETHSavingsAccount.js
+++ test/ETHSavingsAccount.js
@@ -1,10 +1,26 @@
 const ETHSavingsAccount = artifacts.require("./ETHSavingsAccount.sol");
 const utils = require('./utils');
 
+const tokenTypes = {
+  ETH: 0,
+}
 
 contract('ETHSavingsAccount', function(accounts) {
+  var account;
+
+  beforeEach(function() {
+    return ETHSavingsAccount.new().then((instance) => {
+      account = instance;
+    });
+  });
+
+  it("should update the user's balance", async () => {
+    await account.deposit({from: web3.eth.accounts[1], value: 100});
+    const balance = await account.getBalance.call(web3.eth.accounts[1], tokenTypes.ETH);
+    assert.equal(balance.valueOf(), 100);
+  });
+
   it("should create debit and credit ledger entries", async () => {
-    let account = await ETHSavingsAccount.deployed();
     await account.deposit({from: web3.eth.accounts[1], value: 100});
     await utils.assertEvents(account, [
     {
@@ -23,4 +39,5 @@ contract('ETHSavingsAccount', function(accounts) {
     }
     ]);
   });
+
 });
