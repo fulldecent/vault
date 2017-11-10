8b512048d1e0ac517a4a8f2e089c0698104a48f9
Geoffrey Hayes <hayesgm@gmail.com>
2017-11-10 10:26:20 -0800

    Add support assets to Oracle

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -10,7 +10,9 @@ pragma solidity ^0.4.17;
 contract Oracle {
 	address owner;
 	mapping(address => uint64) values;
+	address[] assets;
 
+	event NewAsset(address indexed asset);
 	event AssetValueUpdate(address indexed asset, uint64 valueInWei);
 
 	/**
@@ -29,11 +31,19 @@ contract Oracle {
 	}
 
 	/**
+	  * @dev `getSupportedAssets` returns assets which have Oracle values
+	  * @return assets List of supported asset addresses
+	  */
+	function getSupportedAssets() public view returns(address[]) {
+		return assets;
+	}
+
+	/**
 	  * `getAssetValue` returns the Oracle's view of the current
 	  * value of a given asset, or zero if unknown.
 
 	  * @param asset The address of the asset to query
-	  * @return value (uint64): The value in wei of the asset, or zero.
+	  * @return value The value in wei of the asset, or zero.
 	  */
 	function getAssetValue(address asset) public view returns(uint64) {
 		return values[asset];
@@ -46,10 +56,32 @@ contract Oracle {
 	  * @param valueInWei The value in wei of the asset per unit
 	  */
 	function setAssetValue(address asset, uint64 valueInWei) public onlyOwner {
+		if (!arrayContains(assets, asset)) {
+			assets.push(asset);
+			NewAsset(asset);
+		}
+
 		// Emit log event
 		AssetValueUpdate(asset, valueInWei);
 
 		// Update asset type value
 		values[asset] = valueInWei;
 	}
+
+	/**
+	  * @dev Determines whether or not array contains the given value, here defined as adddresses.
+	  * @param arr Array to check
+	  * @param val Value to look for in `arr`
+	  * @return found Whether or not the value `val` was found in `arr`
+	  */
+	function arrayContains(address[] arr, address val) private pure returns (bool) {
+		for (uint64 i = 0; i < arr.length; i++) {
+			if (arr[i] == val) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
 }
--- test/Oracle.js
+++ test/Oracle.js
@@ -22,14 +22,33 @@ contract('Oracle', function(accounts) {
       const balance = await oracle.getAssetValue.call(tokenAddrs.OMG);
       assert.equal(balance.valueOf(), 15000000000000000000);
 
+      const assets = await oracle.getSupportedAssets.call();
+      assert.equal(assets.valueOf().length, 1);
+      assert.equal(assets.valueOf()[0], tokenAddrs.OMG);
+
       await utils.assertEvents(oracle, [
+      {
+        event: "NewAsset",
+        args: {
+          asset: tokenAddrs.OMG
+        }
+      },
       {
         event: "AssetValueUpdate",
         args: {
           asset: tokenAddrs.OMG,
           valueInWei: web3.toBigNumber('15000000000000000000')
         }
-      }])
+      },
+      ]);
+
+      await oracle.setAssetValue(tokenAddrs.OMG, web3.toWei(14, "ether"), {from: web3.eth.accounts[0]});
+      const balance_2 = await oracle.getAssetValue.call(tokenAddrs.OMG);
+      assert.equal(balance_2.valueOf(), 14000000000000000000);
+
+      const assets_2 = await oracle.getSupportedAssets.call();
+      assert.equal(assets_2.valueOf().length, 1);
+      assert.equal(assets_2.valueOf()[0], tokenAddrs.OMG);
     });
 
     it("should only allow owner to set asset value", async () => {
