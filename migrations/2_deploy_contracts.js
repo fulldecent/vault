9a56a402b99224aa3ee8b5823f4bda798218fcb5
Geoffrey Hayes <hayesgm@gmail.com>
2017-11-15 01:34:36 -0800

    Deploy `EtherToken`, `WalletFactory` and `Bank` contracts on deploy
    This patch updates our deploy code to make sure we deploy all three of the above contracts to the blockchain. We may opt in the future to use an existing `EtherToken` contract, but that's a long term plan. Note: `WalletFactory` depends on both `EtherToken` and `Bank` and thus is deployed last.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 var Bank = artifacts.require("./Bank.sol");
+var EtherToken = artifacts.require("./tokens/EtherToken.sol");
+var WalletFactory = artifacts.require("./WalletFactory.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(Bank);
+module.exports = async function(deployer) {
+  await deployer.deploy(Bank);
+  await deployer.deploy(EtherToken);
+
+  await deployer.deploy(WalletFactory, Bank.address, EtherToken.address);
 };
