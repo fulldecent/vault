6969d37113787146991ba8cbfde9144d747e4313
Geoffrey Hayes <hayesgm@gmail.com>
2017-11-27 12:08:45 -0800

    Move MINIMUM_COLLATERAL_RATIO into a constant

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,8 +2,10 @@ var Vault = artifacts.require("Vault.sol");
 var EtherToken = artifacts.require("EtherToken.sol");
 var WalletFactory = artifacts.require("WalletFactory.sol");
 
+const MINIMUM_COLLATERAL_RATIO = 2;
+
 module.exports = function(deployer) {
-  return deployer.deploy(Vault, 2).then(function() {
+  return deployer.deploy(Vault, MINIMUM_COLLATERAL_RATIO).then(function() {
     return deployer.deploy(EtherToken).then(function() {
       return deployer.deploy(WalletFactory, Vault.address, EtherToken.address);
     });
