5a0194fdce0aa9cd46e1b19fa9898276bdde2f0c
Geoffrey Hayes <hayesgm@gmail.com>
2017-12-11 15:32:06 -0800

    Deploy pig token on Development and Mission networks
    This patch deploys PigToken on Development and Mission networks. This allows us to test a variety of tokens for tests of loans, asset values, etc.

--- addresses.js
+++ addresses.js
@@ -1,20 +1,29 @@
 var Vault = artifacts.require("./Vault.sol");
 var EtherToken = artifacts.require("./tokens/EtherToken.sol");
+var PigToken = artifacts.require("./tokens/PigToken.sol");
 var WalletFactory = artifacts.require("./WalletFactory.sol");
 
 module.exports = async function(callback) {
 	const vault = await Vault.deployed();
 	const etherToken = await EtherToken.deployed();
 	const walletFactory = await WalletFactory.deployed();
+	const tokens = {
+		[etherToken.address]: "eth"
+	};
+
+	try {
+		const pigToken = await PigToken.deployed();
+		tokens[pigToken.address] = "pig";
+	} catch (e) {
+		// Pig token not deployed
+	}
 
 	process.stderr.write(JSON.stringify(
 		{
 			"vault": vault.address,
 			"wallet_factory": walletFactory.address,
 			"ether_token": etherToken.address,
-			"tokens": {
-				[etherToken.address]: "eth"
-			}
+			"tokens": tokens
 		}
 	));
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,20 @@
 var Vault = artifacts.require("Vault.sol");
 var EtherToken = artifacts.require("EtherToken.sol");
+var PigToken = artifacts.require("PigToken.sol");
 var WalletFactory = artifacts.require("WalletFactory.sol");
 
 const MINIMUM_COLLATERAL_RATIO = 2;
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network) {
   return deployer.deploy(Vault, MINIMUM_COLLATERAL_RATIO).then(function() {
     return deployer.deploy(EtherToken).then(function() {
-      return deployer.deploy(WalletFactory, Vault.address, EtherToken.address);
+      return deployer.deploy(WalletFactory, Vault.address, EtherToken.address).then(function() {
+      	if (network == "development" || network == "mission") {
+      		return deployer.deploy(PigToken);
+      	} else {
+      		return true;
+      	}
+      });
     });
   });
 };
