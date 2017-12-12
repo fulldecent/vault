d1b4794edede50400400840b6ab70141d80138cd
Mason Fischer <mason@kissr.co>
2017-12-12 14:20:27 -0800

    Add Token Factory contract
    Other changes:
    
    * Add Solidity Metadata Uploader to upload metadata to swarm
    * Refactor migration file using `async`/`await`

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,20 +1,18 @@
 var Vault = artifacts.require("Vault.sol");
 var EtherToken = artifacts.require("EtherToken.sol");
 var PigToken = artifacts.require("PigToken.sol");
+var TokenFactory = artifacts.require("./TokenFactory.sol");
 var WalletFactory = artifacts.require("WalletFactory.sol");
 
 const MINIMUM_COLLATERAL_RATIO = 2;
 
-module.exports = function(deployer, network) {
-  return deployer.deploy(Vault, MINIMUM_COLLATERAL_RATIO).then(function() {
-    return deployer.deploy(EtherToken).then(function() {
-      return deployer.deploy(WalletFactory, Vault.address, EtherToken.address).then(function() {
-      	if (network == "development" || network == "mission") {
-      		return deployer.deploy(PigToken);
-      	} else {
-      		return true;
-      	}
-      });
-    });
-  });
+module.exports = async function(deployer, network) {
+  await deployer.deploy(TokenFactory);
+  await deployer.deploy(Vault, MINIMUM_COLLATERAL_RATIO);
+  await deployer.deploy(EtherToken);
+  await deployer.deploy(WalletFactory, Vault.address, EtherToken.address);
+
+  if (network == "development" || network == "mission") {
+    await deployer.deploy(PigToken);
+  };
 };
