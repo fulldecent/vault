7e2f38c53f6f32038c005d20913bb700155643ef
Geoffrey Hayes <hayesgm@gmail.com>
2018-01-04 18:45:36 -0800

    Fix Mission network address

--- truffle.js
+++ truffle.js
@@ -51,7 +51,7 @@ module.exports = {
       port: 8545,
       gas: 4612388,
       gasPrice: web3.toWei(15, "gwei"),
-      network_id: "1000",
+      network_id: "235",
       provider: missionProvider,
     },
     ...infuraNetworks,
