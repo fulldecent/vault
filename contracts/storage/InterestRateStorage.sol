55f611280d7c0a50abd83d156d65564b99f7fd8e
Geoffrey Hayes <hayesgm@gmail.com>
2018-01-05 17:28:14 -0800

    Add Interest Rates via Daily Computation
    This patch adds a new Snapshot mechanism to the InterestRateStorage contract. We now track the interest rate each day and what the compound interest would be from that given day to check up to the current day (e.g. principal * getCompoundedInterest(depositTimestamp) = current balance). We do this by storing each day's interest rate (as of midnight) in a mapping from "day # -> Snapshot". Here, day # is the number of days since January 1, 1970 (which is unix timestamp / `60*60*24` truncated). This makes it trivial to find a given snapshot. Each snapshot stores the current timestamp (when it was snapshotted), the current daily interest rate (scaled by 10e9) and the compounded interest rate (that number above). Each time we make a new snapshot, we walk all previous snapshots and update that Snapshot by multiplying its compounded interest rate by the current compounded interest rate to give us the correct multiplier from that day. We keep each number scaled by 10e9 such that this number will not grow unbounded.

--- contracts/storage/InterestRateStorage.sol
+++ contracts/storage/InterestRateStorage.sol
@@ -9,25 +9,154 @@ import "../base/Allowed.sol";
   * @notice Interest rate contract is a simple contract to keep track of interest rates.
   */
 contract InterestRateStorage is Owned, Allowed {
-	// Track assets -> rates
-	mapping(address => uint64) public rates;
+	uint constant secondsPerDay = 86400;
+	uint constant interestRateScale = 10 ** 16;
 
-	event InterestRateChange(address asset, uint64 interestRateBPS);
+	// Track assets -> daily interest rates
+	mapping(address => uint64) public dailyInterestRates;
+
+	struct Snapshot {
+		uint timestamp;
+		uint64 dailyInterestRate;
+		uint256 compoundedInterestRate;
+	}
+
+	// Snapshots map a timestamp to a "rate to date"
+	mapping(address => uint32) public firstSnapshotDays;
+	mapping(address => uint32) public lastSnapshotDays;
+	mapping(address => mapping(uint32 => Snapshot)) public snapshots;
+
+	event InterestRateChange(address asset, uint64 dailyInterestRate);
+	event NewSnapshot(address asset, uint timestamp, uint64 dailyInterestRate);
 
 	/**
-	  * @notice `setInterestRate` sets the interest rate for a given asset
+	  * @notice `getSnapshotTimestamp` returns the timestamp of the first snapshot on or after the given time
+	  * @param asset The asset which was snapshotted
+	  * @param timestamp The timestamp to get the snapshot from
+	  * @return The timestamp of the first snapshot on or after the given time
+	  */
+	function getSnapshotTimestamp(address asset, uint256 timestamp) public view returns (uint256) {
+		return getSnapshot(asset, timestamp).timestamp;
+	}
+
+	/**
+	  * @notice `getSnapshotDailyInterestRate` returns the daily interest rate of the first snapshot on or after the given time
+	  * @param asset The asset which was snapshotted
+	  * @param timestamp The timestamp to get the snapshot from
+	  * @return The daily interest rate of the first snapshot on or after the given time scaled by `interestRateScale`
+	  */
+	function getSnapshotDailyInterestRate(address asset, uint256 timestamp) public view returns (uint64) {
+		return getSnapshot(asset, timestamp).dailyInterestRate;
+	}
+
+	/**
+	  * @notice `getCompoundedInterestRate` returns the compounded interest rate up until now of the first snapshot on or after the given time
+	  * @param asset The asset which was snapshotted
+	  * @param timestamp The timestamp to get the snapshot from
+	  * @return The compounded interest rate for this given asset since timestamp scaled by `interestRateScale`
+	  */
+	function getCompoundedInterestRate(address asset, uint256 timestamp) public view returns (uint256) {
+		return getSnapshot(asset, timestamp).compoundedInterestRate;
+	}
+
+	/**
+	  * @notice `snapshotCurrentRate` takes a daily snapshot of a given asset's rate
+	  * @param asset The asset to snapshot
+	  * @dev This will fail if we have a current snapshot for the given day
+	  * @dev Note: this is public and anyone can call it.
+	  * @return Success or failure of given snapshot.
+	  * TODO: Remove timestamp
+	  */
+	function snapshotCurrentRate(address asset, uint256 timestamp) public returns (bool) {
+		uint32 firstSnapshotDay = firstSnapshotDays[asset];
+		uint32 currentDay = getDay(timestamp);
+		uint64 rate = dailyInterestRates[asset];
+		Snapshot storage existingSnapshot = snapshots[asset][currentDay];
+
+		if (existingSnapshot.timestamp > 0) {
+			failure("InterestRateStorage::RateExists", existingSnapshot.timestamp, existingSnapshot.dailyInterestRate, existingSnapshot.compoundedInterestRate);
+			return false;
+		}
+
+		lastSnapshotDays[asset] = currentDay;
+
+		snapshots[asset][currentDay] = Snapshot(
+			timestamp,
+			rate,
+			interestRateScale
+		);
+
+		NewSnapshot(asset, timestamp, rate);
+
+		if (firstSnapshotDay == 0) {
+			firstSnapshotDays[asset] = currentDay;
+		} else {
+			for (uint32 day = currentDay - 1; day >= firstSnapshotDay; day--) {
+				if (snapshots[asset][day].timestamp == 0) {
+					// Handle the case of filling in missing days
+					snapshots[asset][day] = snapshots[asset][day+1];
+				} else {
+					// Compound interest rate with day
+					snapshots[asset][day].compoundedInterestRate = ( ( interestRateScale + rate ) * snapshots[asset][day].compoundedInterestRate ) / interestRateScale;
+				}
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	  * @notice `getSnapshot` returns the given day's snapshot
+	  * @param asset The asset to snapshot
+	  * @param timestamp The timestamp to get the snapshot from
+	  * @return The timestamp for the given asset based on the day, or the first if timestamp is before the first
+	  */
+	function getSnapshot(address asset, uint256 timestamp) internal view returns (Snapshot) {
+		uint32 day = getDay(timestamp);
+		uint32 firstSnapshotDay = firstSnapshotDays[asset];
+		uint32 lastSnapshotDay = lastSnapshotDays[asset];
+
+		if (firstSnapshotDay == 0) {
+			return Snapshot(
+				0,
+				dailyInterestRates[asset],
+				interestRateScale); // No snapshotted interest
+		}
+
+		if (day < firstSnapshotDay) {
+			day = firstSnapshotDay; // Start from first available day
+		}
+
+		if (day > lastSnapshotDay) {
+			day = lastSnapshotDay; // Go to end if no further days
+		}
+
+		return snapshots[asset][day];
+	}
+
+	/**
+	  * @notice `getDay` returns the day associated with a given timestamp
+	  * @param timestamp A timestamp
+	  * @return The given day number of that timestamp (e.g. timestamp at Jan 1, 1970 = Day 0)
+	  */
+	function getDay(uint timestamp) public pure returns (uint32) {
+		return uint32(timestamp / secondsPerDay);
+	}
+
+	/**
+	  * @notice `setInterestRate` sets the daily interest rate for a given asset
 	  * @param asset The asset to set the interest rate for
-	  * @param interestRateBPS The annual interest rate (APR) in basis points
+	  * @param dailyInterestRate The daily interest rate scaled to `scale` (10e16)
 	  * @return success or failure
 	  */
-	function setInterestRate(address asset, uint64 interestRateBPS) public returns (bool) {
+	function setInterestRate(address asset, uint64 dailyInterestRate) public returns (bool) {
 		if (!checkOwner()) {
 			return false;
 		}
 
-		rates[asset] = interestRateBPS;
+		dailyInterestRates[asset] = dailyInterestRate;
 
-		InterestRateChange(asset, interestRateBPS);
+		InterestRateChange(asset, dailyInterestRate);
 
 		return true;
 	}
@@ -35,10 +164,10 @@ contract InterestRateStorage is Owned, Allowed {
 	/**
 	  * @notice `getInterestRate` returns the interest rate for given asset
 	  * @param asset The asset to get the interest rate for
-	  * @return rate The annual interest rate (APR) in basis points
+	  * @return rate The daily interest rate scaled to `scale` (10e16)
 	  */
 	function getInterestRate(address asset) public view returns (uint64) {
-		return rates[asset];
+		return dailyInterestRates[asset];
 	}
 
 }
--- test/InterestRateStorage.js
+++ test/InterestRateStorage.js
@@ -4,6 +4,8 @@ const EtherToken = artifacts.require("./tokens/EtherToken.sol");
 const utils = require('./utils');
 const moment = require('moment');
 
+const SECONDS_IN_A_DAY = ( 60 * 60 * 24 );
+
 contract('InterestRateStorage', function(accounts) {
   var interestRateStorage;
   var etherToken;
@@ -29,7 +31,7 @@ contract('InterestRateStorage', function(accounts) {
         event: "InterestRateChange",
         args: {
           asset: etherToken.address,
-          interestRateBPS: web3.toBigNumber('500')
+          dailyInterestRate: web3.toBigNumber('500')
         }
       }]);
     });
@@ -38,4 +40,153 @@ contract('InterestRateStorage', function(accounts) {
       await utils.assertOnlyOwner(interestRateStorage, interestRateStorage.setInterestRate.bind(null, etherToken.address, 500), web3);
     });
   });
+
+  describe('#snapshotCurrentRate', async () => {
+    it('should snapshot the first day', async () => {
+      const now = moment().unix() + SECONDS_IN_A_DAY; // since we've moved the EVM forward one day
+      const midnightTonight = now - Math.floor( now / SECONDS_IN_A_DAY ) + 60 * 60 * 24;
+      const secondsUntilMidnight = midnightTonight - now;
+
+      const yesterday = now - SECONDS_IN_A_DAY;
+      const laterYesterday = yesterday + secondsUntilMidnight / 2;
+      const today = now;
+      const laterToday = today + secondsUntilMidnight / 2;
+      const tomorrow = today + SECONDS_IN_A_DAY;
+      const laterTomorrow = tomorrow + secondsUntilMidnight / 2;
+      const theDayAfterTomorrow = tomorrow + SECONDS_IN_A_DAY;
+
+      await interestRateStorage.setInterestRate(etherToken.address, 500);
+      await interestRateStorage.snapshotCurrentRate(etherToken.address, laterToday);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, yesterday)).valueOf(), laterToday);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, yesterday)).valueOf(), 500);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, yesterday)).valueOf(), 10000000000000000);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, laterYesterday)).valueOf(), laterToday);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, laterYesterday)).valueOf(), 500);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, laterYesterday)).valueOf(), 10000000000000000);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, today)).valueOf(), laterToday);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, today)).valueOf(), 500);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, today)).valueOf(), 10000000000000000);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, laterToday)).valueOf(), laterToday);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, laterToday)).valueOf(), 500);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, laterToday)).valueOf(), 10000000000000000);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, tomorrow)).valueOf(), laterToday);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, tomorrow)).valueOf(), 500);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, tomorrow)).valueOf(), 10000000000000000);
+    });
+
+    it('should correctly snapshot a second day', async () => {
+      const now = moment().unix() + SECONDS_IN_A_DAY; // since we've moved the EVM forward one day
+      const midnightTonight = now - Math.floor( now / SECONDS_IN_A_DAY ) + 60 * 60 * 24;
+      const secondsUntilMidnight = midnightTonight - now;
+
+      const yesterday = now - SECONDS_IN_A_DAY;
+      const laterYesterday = yesterday + secondsUntilMidnight / 2;
+      const today = now;
+      const laterToday = today + secondsUntilMidnight / 2;
+      const tomorrow = today + SECONDS_IN_A_DAY;
+
+      await interestRateStorage.setInterestRate(etherToken.address, 500);
+      await interestRateStorage.snapshotCurrentRate(etherToken.address, laterYesterday);
+
+      await interestRateStorage.setInterestRate(etherToken.address, 501);
+      await interestRateStorage.snapshotCurrentRate(etherToken.address, laterToday);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, yesterday)).valueOf(), laterYesterday);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, yesterday)).valueOf(), 500);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, yesterday)).valueOf(), 10000000000000501);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, laterYesterday)).valueOf(), laterYesterday);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, laterYesterday)).valueOf(), 500);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, laterYesterday)).valueOf(), 10000000000000501);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, today)).valueOf(), laterToday);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, today)).valueOf(), 501);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, today)).valueOf(), 10000000000000000);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, laterToday)).valueOf(), laterToday);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, laterToday)).valueOf(), 501);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, laterToday)).valueOf(), 10000000000000000);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, tomorrow)).valueOf(), laterToday);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, tomorrow)).valueOf(), 501);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, tomorrow)).valueOf(), 10000000000000000);
+    });
+
+    it('should correctly snapshot a third day', async () => {
+      const now = moment().unix() + SECONDS_IN_A_DAY; // since we've moved the EVM forward one day
+      const midnightTonight = now - Math.floor( now / SECONDS_IN_A_DAY ) + 60 * 60 * 24;
+      const secondsUntilMidnight = midnightTonight - now;
+
+      const yesterday = now - SECONDS_IN_A_DAY;
+      const laterYesterday = yesterday + secondsUntilMidnight / 2;
+      const today = now;
+      const laterToday = today + secondsUntilMidnight / 2;
+      const tomorrow = today + SECONDS_IN_A_DAY;
+      const laterTomorrow = tomorrow + secondsUntilMidnight / 2;
+      const theDayAfterTomorrow = tomorrow + SECONDS_IN_A_DAY;
+
+      await interestRateStorage.setInterestRate(etherToken.address, 500);
+      await interestRateStorage.snapshotCurrentRate(etherToken.address, laterYesterday);
+
+      await interestRateStorage.setInterestRate(etherToken.address, 501);
+      await interestRateStorage.snapshotCurrentRate(etherToken.address, laterToday);
+
+      await interestRateStorage.setInterestRate(etherToken.address, 490);
+      await interestRateStorage.snapshotCurrentRate(etherToken.address, laterTomorrow);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, yesterday)).valueOf(), laterYesterday);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, yesterday)).valueOf(), 500);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, yesterday)).valueOf(), 10000000000000991);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, laterYesterday)).valueOf(), laterYesterday);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, laterYesterday)).valueOf(), 500);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, laterYesterday)).valueOf(), 10000000000000991);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, today)).valueOf(), laterToday);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, today)).valueOf(), 501);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, today)).valueOf(), 10000000000000490);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, laterToday)).valueOf(), laterToday);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, laterToday)).valueOf(), 501);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, laterToday)).valueOf(), 10000000000000490);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, tomorrow)).valueOf(), laterTomorrow);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, tomorrow)).valueOf(), 490);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, tomorrow)).valueOf(), 10000000000000000);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, laterTomorrow)).valueOf(), laterTomorrow);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, laterTomorrow)).valueOf(), 490);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, laterTomorrow)).valueOf(), 10000000000000000);
+
+      assert.equal((await interestRateStorage.getSnapshotTimestamp(etherToken.address, theDayAfterTomorrow)).valueOf(), laterTomorrow);
+      assert.equal((await interestRateStorage.getSnapshotDailyInterestRate(etherToken.address, theDayAfterTomorrow)).valueOf(), 490);
+      assert.equal((await interestRateStorage.getCompoundedInterestRate(etherToken.address, theDayAfterTomorrow)).valueOf(), 10000000000000000);
+    });
+
+    it('should correctly snapshot a fourth day missing a third day');
+    it('should be allowed only');
+  });
+
+  describe('#getDay', async () => {
+    it('should return 0 for Jan 1, 1970', async () => {
+      var timestamp = moment("01-01-1970", "MM-DD-YYYY").unix();
+
+      assert.equal((await interestRateStorage.getDay.call(timestamp)).valueOf(), 0);
+    });
+    it('should return 1 for Jan 2, 1970', async () => {
+      var timestamp = moment("01-02-1970", "MM-DD-YYYY").unix();
+
+      assert.equal((await interestRateStorage.getDay.call(timestamp)).valueOf(), 1);
+    });
+    it('should return correctly for July 1, 2073', async () => {
+      var timestamp = moment("07-01-2073", "MM-DD-YYYY").unix();
+
+      assert.equal((await interestRateStorage.getDay.call(timestamp)).valueOf(), 37802);
+    });
+  })
 });
