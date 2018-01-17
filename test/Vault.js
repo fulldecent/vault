53ba3f22d19d7592305387e4e556e0cc6bb5a44d
Art Peel <foundart@gmail.com>
2018-01-17 12:18:43 -0800

    add test definitions for some new enums

--- test/Vault.js
+++ test/Vault.js
@@ -25,6 +25,7 @@ const LedgerReason = {
   Interest: web3.toBigNumber(2),
   CustomerBorrow: web3.toBigNumber(3),
   CustomerPayLoan: web3.toBigNumber(4),
+  CollateralPayLoan: web3.toBigNumber(5),
 };
 
 const LedgerAccount = {
@@ -32,7 +33,8 @@ const LedgerAccount = {
   Loan: web3.toBigNumber(1),
   Deposit: web3.toBigNumber(2),
   InterestExpense: web3.toBigNumber(3),
-  InterestIncome: web3.toBigNumber(4)
+  InterestIncome: web3.toBigNumber(4),
+  Trading: web3.toBigNumber(5),
 };
 
 contract('Vault', function(accounts) {
