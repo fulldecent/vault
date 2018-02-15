e31fa91f9aba08fcc7f55557c84aa27a2d0cc7ed
Geoffrey Hayes <hayesgm@gmail.com>
2018-02-15 14:39:00 -0800

    Minor stylistic updates

--- contracts/tokens/FaucetToken.sol
+++ contracts/tokens/FaucetToken.sol
@@ -9,59 +9,69 @@ import "./../base/StandardToken.sol";
   * @notice A simple token that lets anyone get more of it, with a cap on the amount that can be allocated per request
   */
 contract FaucetToken is StandardToken, Owned {
+    string constant public name = "Pig Token";
+    string constant public symbol = "PIG";
+    uint8 constant public decimals = 16;
 
-	string constant public name = "Pig Token";
-	string constant public symbol = "PIG";
-	uint8 constant public decimals = 16;
+    uint256 public perRequestTokenAmount;
 
-	uint256 public perRequestTokenAmount;
+    /**
+      * @notice Arbitrarily adds configured quantity of tokens to account of msg.sender
+      * @dev This is for automated testing and for convenience on the alpha test net
+      * @return Success or failure of operation
+      */
+    function allocate() public returns (bool) {
 
-	/**
-	  * @notice Arbitrarily adds configured quantity of tokens to account of msg.sender
-	  * @dev This is for automated testing and for convenience on the alpha test net
-	  */
-	function allocate() public returns (bool){
+        if (perRequestTokenAmount == 0) {
+            return failure("FaucetToken::AssetNotDisbursable");
+        }
 
-		if(perRequestTokenAmount == 0) {
-			return failure("FaucetToken::AssetNotDisbursable");
-		}
+        return doAllocation(msg.sender, perRequestTokenAmount);
+    }
 
-		return doAllocation(msg.sender, perRequestTokenAmount);
-	}
-
-	/**
-	  * @notice Arbitrarily adds tokens to account
-	  * @dev This is for automated testing and for convenience on the alpha test net
-	  * @param recipient Account to add tokens to.
-	  * @param value Amount to add
+    /**
+      * @notice Arbitrarily adds tokens to account
+      * @dev This is for automated testing and for convenience on the alpha test net
+      * @param recipient Account to add tokens to.
+      * @param value Amount to add
+      * @return Success or failure of operation
     */
-	function allocateTo(address recipient, uint256 value) public returns (bool){
+    function allocateTo(address recipient, uint256 value) public returns (bool) {
+        if (!checkOwner()) {
+            return false;
+        }
 
-		if(!checkOwner()) {
-			return false;
-		}
-		return doAllocation(recipient, value);
-	}
+        return doAllocation(recipient, value);
+    }
 
-	function doAllocation(address _owner, uint256 value) internal returns (bool) {
-		balances[_owner] += value;
-		totalSupply += value;
-		Transfer(address(this), _owner, value);
-		return true;
-	}
+    /**
+      * @notice Internal function to actually preform token allocation
+      * @dev This is for automated testing and for convenience on the alpha test net
+      * @param _owner Account to add tokens to.
+      * @param value Amount to add.
+      * @return Success or failure of operation
+    */
+    function doAllocation(address _owner, uint256 value) internal returns (bool) {
+        balances[_owner] += value;
+        totalSupply += value;
+        Transfer(address(this), _owner, value);
 
-	/**
-	  * @notice `setPerRequestTokenAmount` allows the contract owner to set/update the amount given for each request of the specified token
-	  * @dev This is for convenience on alpha test net
-	  * @param amount How much of the token should be given out? Set to 0 to disallow allocations
-	  * @return Success of failure of operation
-	  */
-	function setPerRequestTokenAmount(uint256 amount) public returns (bool) {
-		if (!checkOwner()) {
-			return false;
-		}
+        return true;
+    }
 
-		perRequestTokenAmount = amount;
-		return true;
-	}
+    /**
+      * @notice `setPerRequestTokenAmount` allows the contract owner to set/update the amount given for each request of the specified token
+      * @dev This is for convenience on alpha test net
+      * @param amount How much of the token should be given out? Set to 0 to disallow allocations
+      * @return Success of failure of operation
+      */
+    function setPerRequestTokenAmount(uint256 amount) public returns (bool) {
+        if (!checkOwner()) {
+            return false;
+        }
+
+        perRequestTokenAmount = amount;
+        return true;
+    }
+
 }
